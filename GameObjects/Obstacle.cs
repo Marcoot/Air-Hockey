48745e55caee44c47794b3f2721757be8995bf9a
Marco <marco.bosker@hva.nl>
2022-07-05 00:46:51 +0200

    stap 1: obstacle heeft een class met een positie en een sprite

---
+++ GameObjects/Obstacle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace SimPhy_Jun2022.GameObjects
+{
+    class Obstacle : SpriteGameObject
+    {
+        public Obstacle(Vector2 position) : base("spr_obstacle")
+        {
+            this.position = position;
+        }
+    }
+}
