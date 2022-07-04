45e48dbdf62570b1b15e77b2e5361f248cb6bf10
Marco <marco.bosker@hva.nl>
2022-07-05 00:35:15 +0200

    stap 1: wall heeft een class en kan een startpositie hebben en een gekozen sprite kan toegevoegd worden

---
+++ GameObjects/Wall.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace SimPhy_Jun2022.GameObjects
+{
+    class Wall : SpriteGameObject
+    {
+        //wall met in argument een positie
+        public Wall(Vector2 position, String assetName) : base(assetName)
+        {
+            this.position = position;
+        }
+    }
+}
