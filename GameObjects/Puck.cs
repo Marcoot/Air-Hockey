fa1835d346d42ab14ccc297ac5641474fdeb27da
Marco <marco.bosker@hva.nl>
2022-07-05 00:24:47 +0200

    stap 1: puck heeft een class waar die een startpositie kan hebben met een zelfgemaakte sprite

---
+++ GameObjects/Puck.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace SimPhy_Jun2022.GameObjects
+{
+    class Puck : SpriteGameObject
+    {
+        public Puck(Vector2 startPosition) : base("spr_puck")
+        {
+            this.position = startPosition;
+        }
+    }
+}
