6f20e659e79b491874cc946db321438735786eaf
Marco <marco.bosker@hva.nl>
2022-07-05 00:43:28 +0200

    stap 1: pusher heeft een class met een startpositie en een sprite

---
+++ GameObjects/Pusher.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace SimPhy_Jun2022.GameObjects
+{
+    class Pusher : SpriteGameObject
+    {
+        public Pusher(Vector2 startPosition) : base("spr_pusher")
+        {
+            this.position = startPosition;
+        }
+    }
+}
